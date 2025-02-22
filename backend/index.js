f07beaf08fa9c67709b307d425bbd7a247a6c016
arunpravin125 <arunpravin125@gmail.com>
2025-02-23 00:25:29 +0530

    deployment

--- backend/index.js
+++ backend/index.js
@@ -7,18 +7,27 @@ import cookieParser from "cookie-parser"
 import { notificationRoute } from "./routes/notification.route.js"
 import { app, server } from "./socket/socket.js"
 import { groupConversations } from "./routes/groupConversation.route.js"
-
+import path from "path"
 
 app.use(express.json())
 dotenv.config()
 app.use(cookieParser())
 const PORT = process.env.PORT || 3501
+const __dirname = path.resolve()
+
 
 app.use("/api/auth",authRoute)
 app.use("/api/message",messageRoute)
 app.use("/api/notification",notificationRoute)
 app.use("/api/groupConversation",groupConversations)
 
+if(process.env.NODE_ENV == "production"){
+    app.use(express.static(path.join(__dirname,"/frontend/dist")))
+
+    app.get("*",(req,res)=>{
+        res.sendFile(path.resolve(__dirname,"frontend","dist","index.html"))
+    })
+}
 
 server.listen(PORT,()=>{
     console.log("Server running",PORT)
